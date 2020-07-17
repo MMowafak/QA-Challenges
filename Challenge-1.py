2fa767bb688223aab021938b5fdb3386eedefe1d
MMowafak <Mowafak399@hotmail.com>
2020-07-17 09:05:55 +0100

    The First Challenge

---
+++ Challenge-1.py
@@ -0,0 +1,5 @@
+
+First_name = ['chris', 'john', 'jason', 'ryan', 'bradley', 'javas', 'thembi', 'jacob', 'chifford', 'wasim', 'mowafak', 'tobi', 'chris', 'samuel', 'ed', 'amanda', 'steven','jack']
+First_name.append('luke')
+print(First_name[4])
+print(First_name.count('chris'))
